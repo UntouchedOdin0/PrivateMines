7cc54adfe231a89b0d6cdf6cac058c8eba35080c
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-03-09 22:49:00 +0000

    Add MineConfig and MineType back in

---
+++ src/main/java/me/untouchedodin0/privatemines/config/MineConfig.java
@@ -0,0 +1,12 @@
+package me.untouchedodin0.privatemines.config;
+
+import me.untouchedodin0.privatemines.configmanager.annotations.ConfigMappable;
+import me.untouchedodin0.privatemines.type.MineType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ConfigMappable
+public class MineConfig {
+    public static Map<String, MineType> mineTypes = new HashMap<>();
+}
