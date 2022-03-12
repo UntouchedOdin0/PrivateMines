d06c69bb501f76eda20b709521ecf292050b48a3
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-03-12 16:22:40 +0000

    pasting the mines async :)

--- src/main/java/me/untouchedodin0/privatemines/factory/MineFactory.java
+++ src/main/java/me/untouchedodin0/privatemines/factory/MineFactory.java
@@ -17,6 +17,7 @@ import me.untouchedodin0.privatemines.PrivateMines;
 import me.untouchedodin0.privatemines.iterator.SchematicIterator;
 import me.untouchedodin0.privatemines.storage.SchematicStorage;
 import me.untouchedodin0.privatemines.type.MineType;
+import me.untouchedodin0.privatemines.utils.task.Task;
 import org.bukkit.Location;
 
 import java.io.File;
@@ -33,45 +34,47 @@ public class MineFactory {
 
     public void create(Location location, MineType mineType) {
         File schematicFile = new File("plugins/PrivateMines/schematics/" + mineType.getFile());
-        Clipboard clipboard;
         ClipboardFormat clipboardFormat = ClipboardFormats.findByFile(schematicFile);
         BlockVector3 blockVector3 = BlockVector3.at(location.getBlockX(), location.getBlockY(), location.getBlockZ());
         schematicStorage = privateMines.getSchematicStorage();
         SchematicIterator.MineBlocks mineBlocks = schematicStorage.getMineBlocksMap().get(schematicFile);
 
-        if (clipboardFormat != null) {
-            try (ClipboardReader clipboardReader = clipboardFormat.getReader(new FileInputStream(schematicFile))) {
-                clipboard = clipboardReader.read();
+        Task task = Task.asyncDelayed(() -> {
+            Clipboard clipboard;
+            if (clipboardFormat != null) {
+                try (ClipboardReader clipboardReader = clipboardFormat.getReader(new FileInputStream(schematicFile))) {
+                    clipboard = clipboardReader.read();
 
-                World world = BukkitAdapter.adapt(Objects.requireNonNull(location.getWorld()));
-                EditSession editSession = WorldEdit.getInstance().newEditSessionBuilder().world(world).build();
-                privateMines.getLogger().info("clipboard: " + clipboard);
-                privateMines.getLogger().info("edit session: " + editSession);
-                privateMines.getLogger().info("blockVector3: " + blockVector3);
-                privateMines.getLogger().info("The spawn vector for " + schematicFile.getName() + " is " + mineBlocks.getSpawnLocation());
-                privateMines.getLogger().info("The corner 1 vector for " + schematicFile.getName() + " is " + mineBlocks.getCorner1());
-                privateMines.getLogger().info("The corner 2 vector for " + schematicFile.getName() + " is " + mineBlocks.getCorner2());
+                    World world = BukkitAdapter.adapt(Objects.requireNonNull(location.getWorld()));
+                    EditSession editSession = WorldEdit.getInstance().newEditSessionBuilder().world(world).build();
+                    privateMines.getLogger().info("clipboard: " + clipboard);
+                    privateMines.getLogger().info("edit session: " + editSession);
+                    privateMines.getLogger().info("blockVector3: " + blockVector3);
+                    privateMines.getLogger().info("The spawn vector for " + schematicFile.getName() + " is " + mineBlocks.getSpawnLocation());
+                    privateMines.getLogger().info("The corner 1 vector for " + schematicFile.getName() + " is " + mineBlocks.getCorner1());
+                    privateMines.getLogger().info("The corner 2 vector for " + schematicFile.getName() + " is " + mineBlocks.getCorner2());
 
-                Instant start = Instant.now();
+                    Instant start = Instant.now();
 
-                Operation operation = new ClipboardHolder(clipboard)
-                        .createPaste(editSession)
-                        .to(blockVector3)
-                        .ignoreAirBlocks(true)
-                        .build();
-                try {
-                    Operations.complete(operation);
-                    editSession.close();
-                } catch (WorldEditException worldEditException) {
-                    worldEditException.printStackTrace();
+                    Operation operation = new ClipboardHolder(clipboard)
+                            .createPaste(editSession)
+                            .to(blockVector3)
+                            .ignoreAirBlocks(true)
+                            .build();
+                    try {
+                        Operations.complete(operation);
+                        editSession.close();
+                    } catch (WorldEditException worldEditException) {
+                        worldEditException.printStackTrace();
+                    }
+
+                    Instant end = Instant.now();
+                    Duration timeElapsedStream = Duration.between(start, end);
+                    privateMines.getLogger().info("Time elapsed: " + timeElapsedStream.toMillis() + "ms");
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
-                Instant end = Instant.now();
-                Duration timeElapsedStream = Duration.between(start, end);
-                privateMines.getLogger().info("Time elapsed: " + timeElapsedStream.toMillis() + "ms");
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-        }
+        });
     }
 }
