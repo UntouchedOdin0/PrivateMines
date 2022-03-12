b224e705ac998e2423d8b3601df3d35a144a4783
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-03-12 20:01:36 +0000

    changes (need to fix the location stuffs)........

--- src/main/java/me/untouchedodin0/privatemines/factory/MineFactory.java
+++ src/main/java/me/untouchedodin0/privatemines/factory/MineFactory.java
@@ -1,6 +1,7 @@
 package me.untouchedodin0.privatemines.factory;
 
 import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.MaxChangedBlocksException;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
@@ -10,9 +11,11 @@ import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
 import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.function.pattern.Pattern;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.world.World;
+import com.sk89q.worldedit.world.block.BlockTypes;
 import me.untouchedodin0.privatemines.PrivateMines;
 import me.untouchedodin0.privatemines.iterator.SchematicIterator;
 import me.untouchedodin0.privatemines.storage.SchematicStorage;
@@ -32,10 +35,15 @@ public class MineFactory {
     PrivateMines privateMines = PrivateMines.getPrivateMines();
     SchematicStorage schematicStorage;
 
+    BlockVector3 spawn;
+    BlockVector3 corner1;
+    BlockVector3 corner2;
+
     public void create(Location location, MineType mineType) {
         File schematicFile = new File("plugins/PrivateMines/schematics/" + mineType.getFile());
         ClipboardFormat clipboardFormat = ClipboardFormats.findByFile(schematicFile);
         BlockVector3 blockVector3 = BlockVector3.at(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+
         schematicStorage = privateMines.getSchematicStorage();
         SchematicIterator.MineBlocks mineBlocks = schematicStorage.getMineBlocksMap().get(schematicFile);
 
@@ -53,6 +61,12 @@ public class MineFactory {
                     privateMines.getLogger().info("The spawn vector for " + schematicFile.getName() + " is " + mineBlocks.getSpawnLocation());
                     privateMines.getLogger().info("The corner 1 vector for " + schematicFile.getName() + " is " + mineBlocks.getCorner1());
                     privateMines.getLogger().info("The corner 2 vector for " + schematicFile.getName() + " is " + mineBlocks.getCorner2());
+                    this.spawn = mineBlocks.getSpawnLocation();
+                    this.corner1 = mineBlocks.getCorner1();
+                    this.corner2 = mineBlocks.getCorner2();
+
+                    Location location1 = BukkitAdapter.adapt(location.getWorld(), corner1);
+                    Location location2 = BukkitAdapter.adapt(location.getWorld(), corner2);
 
                     Instant start = Instant.now();
 
@@ -61,6 +75,16 @@ public class MineFactory {
                             .to(blockVector3)
                             .ignoreAirBlocks(true)
                             .build();
+
+                    try {
+                        editSession.setBlock(spawn, (Pattern) BlockTypes.BARREL);
+                    } catch (MaxChangedBlocksException e) {
+                        e.printStackTrace();
+                    }
+                    Location spawnLocation = BukkitAdapter.adapt(location.getWorld(), blockVector3.subtract(spawn));
+                    Location corner1Location = BukkitAdapter.adapt(location.getWorld(), blockVector3.subtract(corner1));
+                    Location corner2Location = BukkitAdapter.adapt(location.getWorld(), blockVector3.subtract(corner2));
+
                     try {
                         Operations.complete(operation);
                         editSession.close();
@@ -71,6 +95,10 @@ public class MineFactory {
                     Instant end = Instant.now();
                     Duration timeElapsedStream = Duration.between(start, end);
                     privateMines.getLogger().info("Time elapsed: " + timeElapsedStream.toMillis() + "ms");
+                    privateMines.getLogger().info("spawnLocation: " + spawnLocation);
+                    privateMines.getLogger().info("corner1Location: " + corner1Location);
+                    privateMines.getLogger().info("corner2Location: " + corner2Location);
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
