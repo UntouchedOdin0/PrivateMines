d1b9bb8114440c0cb5106c07da911cc90bb18db1
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-03-13 22:34:37 +0000

    changes (credit to dkim19375)

--- src/main/java/me/untouchedodin0/privatemines/utils/Utils.java
+++ src/main/java/me/untouchedodin0/privatemines/utils/Utils.java
@@ -9,18 +9,18 @@ import org.bukkit.Location;
 public class Utils {
 
     public static Location getRelative(Region region, int x, int y, int z) {
-        final BlockVector3 point = region.getMinimumPoint();
+        final BlockVector3 point = region.getMinimumPoint().getMinimum(region.getMaximumPoint());
         final int regionX = point.getX();
         final int regionY = point.getY();
         final int regionZ = point.getZ();
-        final BlockVector3 maxPoint = region.getMaximumPoint();
+        final BlockVector3 maxPoint = region.getMaximumPoint().getMaximum(region.getMinimumPoint());
         final int maxX = maxPoint.getX();
         final int maxY = maxPoint.getY();
         final int maxZ = maxPoint.getZ();
-        if (x < 0 || y < 0 || z < 0
-                || x > maxX - regionX || y > maxY - regionY || z > maxZ - regionZ) {
-            throw new IndexOutOfBoundsException("Relative location outside bounds of structure: " + x + ", " + y + ", " + z);
-        }
+//        if (x < 0 || y < 0 || z < 0
+//                || x > maxX - regionX || y > maxY - regionY || z > maxZ - regionZ) {
+//            throw new IndexOutOfBoundsException("Relative location outside bounds of structure: " + x + ", " + y + ", " + z);
+//        }
         final World worldeditWorld = region.getWorld();
         final org.bukkit.World bukkitWorld;
         if (worldeditWorld != null) {
