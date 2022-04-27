fff7b09c9f1a2be9be1a6856ee43809d753d822f
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-04-27 22:11:59 +0100

    not so smooth, I forgot to clean this up

--- src/main/java/me/untouchedodin0/privatemines/PrivateMines.java
+++ src/main/java/me/untouchedodin0/privatemines/PrivateMines.java
@@ -43,7 +43,6 @@ import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
