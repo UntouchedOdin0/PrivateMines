b0b1665d781e961216a7885dd1854846edde8000
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-06-02 22:03:53 +0100

    ?

--- src/main/java/me/untouchedodin0/privatemines/iterator/SchematicIterator.java
+++ src/main/java/me/untouchedodin0/privatemines/iterator/SchematicIterator.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2021 - 2022 Kyle Hicks
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -62,12 +62,12 @@ public class SchematicIterator {
                 clipboard = clipboardReader.read();
 
                 Material cornerMaterial = Config.mineCorner;
-                Material spawnMaterial  = Config.spawnPoint;
-                Material npcMaterial    = Config.sellNpc;
+                Material spawnMaterial = Config.spawnPoint;
+                Material npcMaterial = Config.sellNpc;
 
                 BlockType cornerType = BlockType.REGISTRY.get(cornerMaterial.getKey().getKey());
-                BlockType spawnType  = BlockType.REGISTRY.get(spawnMaterial.getKey().getKey());
-                BlockType npcType    = BlockType.REGISTRY.get(npcMaterial.getKey().getKey());
+                BlockType spawnType = BlockType.REGISTRY.get(spawnMaterial.getKey().getKey());
+                BlockType npcType = BlockType.REGISTRY.get(npcMaterial.getKey().getKey());
 
                 clipboard.getRegion().forEach(blockVector3 -> {
                     BlockType blockType = clipboard.getBlock(blockVector3).getBlockType();
@@ -90,6 +90,7 @@ public class SchematicIterator {
                 });
 
                 mineBlocks.spawnLocation = BlockVector3.at(spawn.getX(), spawn.getY(), spawn.getZ());
+                mineBlocks.npcLocation = BlockVector3.at(npc.getX(), npc.getY(), npc.getZ());
                 mineBlocks.corners[0] = BlockVector3.at(corner1.getX(), corner1.getY(), corner1.getZ());
                 mineBlocks.corners[1] = BlockVector3.at(corner2.getX(), corner2.getY(), corner2.getZ());
 
@@ -116,6 +117,7 @@ public class SchematicIterator {
         public BlockVector3 getNpcLocation() {
             return npcLocation;
         }
+
         public BlockVector3 getCorner1() {
             return corners[0];
         }
