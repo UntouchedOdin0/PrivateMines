cb3069e4cc3313cbff4cb9868d56d2b39bb9b218
Jesse McKee <jessemckee97@gmail.com>
2022-09-22 18:08:34 -0500

    Add Kotlin Style builder

---
+++ src/main/kotlin/me/untouchedodin0/kotlin/utils/BuilderUtils.kt
@@ -0,0 +1,15 @@
+package me.untouchedodin0.kotlin.utils
+
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import redempt.redlib.itemutils.ItemBuilder
+
+class BuilderUtils {
+
+    inline fun itemBuilder(material: Material, builderItem : ItemBuilder.() -> Unit): ItemStack? {
+        val builder = ItemBuilder(material)
+        builder.builderItem()
+        return builder.toItemStack()
+    }
+
+}
