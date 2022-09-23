8a5f213df14bd3e4bf3dbd2b84567736d92960cd
Jesse McKee <jessemckee97@gmail.com>
2022-09-23 03:05:32 -0500

    Created a KT class for MineData, functions as the builder as well. This was written to maintain compatibility with current code

---
+++ src/main/kotlin/me/untouchedodin0/kotlin/mine/data/MineData.kt
@@ -0,0 +1,49 @@
+package me.untouchedodin0.kotlin.mine.data
+
+import me.untouchedodin0.kotlin.mine.type.MineType
+import me.untouchedodin0.privatemines.playershops.Shop
+import org.bukkit.Location
+import org.bukkit.Material
+import java.util.EnumMap
+import java.util.UUID
+
+data class MineData(
+    val mineOwner: UUID,
+    var maximumMining: Location,
+    var minimumMining: Location,
+    var minimumFullRegion: Location,
+    var maximumFullRegion: Location,
+    val mineLocation: Location,
+    val mineType: MineType,
+    var isOpen: Boolean = false,
+    var tax: Double = 5.0,
+) {
+    var shop: Shop? = null
+    var spawnLocation: Location? = null
+
+    var bannedPlayers: MutableList<UUID> = mutableListOf()
+    var materials: Map<Material, Double> = EnumMap(Material::class.java)
+
+    constructor(
+        mineOwner: UUID,
+        maximumMining: Location,
+        minimumMining: Location,
+        minimumFullRegion: Location,
+        maximumFullRegion: Location,
+        spawnLocation: Location,
+        mineLocation: Location,
+        mineType: MineType,
+        shop: Shop
+    ) : this(
+        mineOwner,
+        maximumMining,
+        minimumMining,
+        minimumFullRegion,
+        maximumFullRegion,
+        mineLocation,
+        mineType
+    ) {
+        this.spawnLocation = spawnLocation
+        this.shop = shop
+    }
+}
