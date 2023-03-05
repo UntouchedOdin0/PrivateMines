4647f243de9afdeeaf87d965790d411e4ff94a23
UntouchedOdin0 <kylehicksuk@gmail.com>
2023-03-05 22:35:49 +0000

    Public mines command changes

--- src/main/java/me/untouchedodin0/privatemines/commands/PublicMinesCommand.java
+++ src/main/java/me/untouchedodin0/privatemines/commands/PublicMinesCommand.java
@@ -9,10 +9,12 @@ import dev.triumphteam.gui.guis.Gui;
 import dev.triumphteam.gui.guis.GuiItem;
 import dev.triumphteam.gui.guis.ScrollingGui;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 import me.untouchedodin0.kotlin.mine.data.MineData;
 import me.untouchedodin0.kotlin.mine.storage.MineStorage;
 import me.untouchedodin0.privatemines.PrivateMines;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -24,18 +26,25 @@ public class PublicMinesCommand extends BaseCommand {
   PrivateMines privateMines = PrivateMines.getPrivateMines();
   MineStorage mineStorage = privateMines.getMineStorage();
 
-  //todo make this work!
   @Default
   public void defaultCommand(Player player) {
+    AtomicInteger openAmount = new AtomicInteger();
+
     ScrollingGui scrollingGui = Gui.scrolling().title(Component.text("Public Mines")).rows(6)
         .pageSize(45).scrollType(ScrollType.VERTICAL).create();
 
-    GuiItem back = ItemBuilder.from(Material.ARROW).asGuiItem(event -> {
+    GuiItem back = ItemBuilder.from(Material.ARROW)
+        .name(Component.text(ChatColor.GREEN + "<-"))
+        .lore(Component.text(ChatColor.GRAY + "Go Back"))
+        .asGuiItem(event -> {
       event.setCancelled(true);
       scrollingGui.previous();
     });
 
-    GuiItem forward = ItemBuilder.from(Material.ARROW).asGuiItem(event -> {
+    GuiItem forward = ItemBuilder.from(Material.ARROW)
+        .name(Component.text(ChatColor.GREEN + "->"))
+        .lore(Component.text(ChatColor.GRAY + "Go Forward"))
+        .asGuiItem(event -> {
       event.setCancelled(true);
       scrollingGui.next();
     });
@@ -43,7 +52,14 @@ public class PublicMinesCommand extends BaseCommand {
     mineStorage.getMines().forEach((uuid, mine) -> {
       MineData mineData = mine.getMineData();
       boolean isOpen = mineData.isOpen();
+      if (isOpen) {
+        openAmount.getAndIncrement();
+      }
+    });
 
+    mineStorage.getMines().forEach((uuid, mine) -> {
+      MineData mineData = mine.getMineData();
+      boolean isOpen = mineData.isOpen();
       if (isOpen) {
         UUID owner = mineData.getMineOwner();
         String name = Bukkit.getOfflinePlayer(owner).getName();
@@ -59,16 +75,15 @@ public class PublicMinesCommand extends BaseCommand {
         }
       }
     });
-//    GuiItem guiItem = ItemBuilder.from(Material.EMERALD).asGuiItem(event -> {
-//      event.setCancelled(true);
-//    });
-//
-//    for (int i = 0; i < 60; i++) {
-//      scrollingGui.addItem(guiItem);
-//    }
 
-    scrollingGui.setItem(48, back);
-    scrollingGui.setItem(50, forward);
-    scrollingGui.open(player);
+    if (openAmount.get() > 0) {
+      scrollingGui.setItem(48, back);
+      scrollingGui.setItem(50, forward);
+      scrollingGui.open(player);
+    } else {
+      Gui gui = Gui.gui().title(Component.text("No Open Mines").color(NamedTextColor.DARK_RED))
+          .create();
+      gui.open(player);
+    }
   }
 }
