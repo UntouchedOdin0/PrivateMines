fd069eb2f48ca101fb07128d2cd92001dea97efd
UntouchedOdin0 <kylehicksuk@gmail.com>
2023-04-07 15:02:06 +0100

    Removes unused imports

--- src/main/java/me/untouchedodin0/privatemines/storage/sql/Database.java
+++ src/main/java/me/untouchedodin0/privatemines/storage/sql/Database.java
@@ -1,30 +1,9 @@
 package me.untouchedodin0.privatemines.storage.sql;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import me.untouchedodin0.privatemines.PrivateMines;
-import me.untouchedodin0.privatemines.storage.sql.errors.Error;
 
 public abstract class Database {
-  PrivateMines privateMines = PrivateMines.getPrivateMines();
   Connection connection;
-  String databaseName = "privatemines";
 
   public abstract Connection getSQLConnection();
-
-  public void close(PreparedStatement preparedStatement, ResultSet resultSet) {
-    try {
-      if (preparedStatement != null) {
-        preparedStatement.close();
-      }
-      if (resultSet != null) {
-        resultSet.close();
-      }
-    } catch (SQLException ex) {
-      Error.close(privateMines, ex);
-    }
-  }
 }
--- src/main/java/me/untouchedodin0/privatemines/storage/sql/SQLite.java
+++ src/main/java/me/untouchedodin0/privatemines/storage/sql/SQLite.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.logging.Level;
 import me.untouchedodin0.privatemines.PrivateMines;
 
