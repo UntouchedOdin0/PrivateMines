51f412a706f822a4c7879875bc7194ff8b4b53b1
UntouchedOdin0 <kylehicksuk@gmail.com>
2024-07-28 22:25:24 +0100

    Tidied up PlayerShopListener

--- src/main/java/me/untouchedodin0/privatemines/listener/sell/PlayerShopListener.java
+++ src/main/java/me/untouchedodin0/privatemines/listener/sell/PlayerShopListener.java
@@ -1,7 +1,5 @@
 package me.untouchedodin0.privatemines.listener.sell;
 
-import java.util.Map;
-import java.util.Objects;
 import me.untouchedodin0.kotlin.mine.data.MineData;
 import me.untouchedodin0.kotlin.mine.storage.MineStorage;
 import me.untouchedodin0.privatemines.PrivateMines;
@@ -9,10 +7,8 @@ import me.untouchedodin0.privatemines.mine.Mine;
 import me.untouchedodin0.privatemines.playershops.Shop;
 import me.untouchedodin0.privatemines.playershops.ShopItem;
 import me.untouchedodin0.privatemines.playershops.ShopUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -25,20 +21,16 @@ public class PlayerShopListener implements Listener {
 
   @EventHandler(priority = EventPriority.LOWEST)
   public void onBlockBreak(BlockBreakEvent event) {
-    Player player = event.getPlayer();
     Location location = event.getBlock().getLocation();
     Mine mine = mineStorage.getClosest(location);
     if (mine != null) {
       MineData mineData = mine.getMineData();
       Shop shop = mineData.getShop();
-      ShopUtils shopUtils = new ShopUtils();
       Material material = location.getBlock().getType();
       int quantity = event.getBlock().getDrops().size();
       double price = shop != null ? shop.getPrice(material) : 0;
       ShopItem shopItem = new ShopItem(material, quantity, price, 0);
       ShopUtils.addItem(mineData.getMineOwner(), shopItem);
-//      ShopUtils.addItem(player.getUniqueId(), material, quantity, price);
-      //shopUtils.sellItems(player.getUniqueId(), true);
     }
   }
 }
